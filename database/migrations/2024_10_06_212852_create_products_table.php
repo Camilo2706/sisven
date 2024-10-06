5da16f89f205facd883757ef36d82bc8d2726657
CAMILO HOYOS <camilohoyos2706@gmail.com>
2024-10-06 16:43:49 -0500

    ARREGLOS TABLES

--- database/migrations/2024_10_06_212852_create_products_table.php
+++ database/migrations/2024_10_06_212852_create_products_table.php
@@ -12,11 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');  
+            $table->char('name', 80);  
             $table->decimal('price', 8, 2);  
+            $table->integer('stock');  
             $table->unsignedBigInteger('category_id');  
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade'); 
             $table->timestamps();
         });
     }
